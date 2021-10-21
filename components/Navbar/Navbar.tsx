712a8a4c4d2e3a4731fb07cbf5d4fc450df4a0f2
Marouane Souda <marsou431@gmail.com>
2021-10-21 18:56:34 +0100

    setup project

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.header``;
+
+function Navbar() {
+    return (
+        <Container>
+            Hello world!
+        </Container>
+    )
+}
+
+export default Navbar;
